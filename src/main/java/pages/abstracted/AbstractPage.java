8061520c11beab52c671f8ffec451ca6042c938c
Henry Darana <h.darana@gmail.com>
2024-06-08 23:47:44 +0530

    Feature: Now we can run on RemoteWebDriver

--- src/main/java/pages/abstracted/AbstractPage.java
+++ src/main/java/pages/abstracted/AbstractPage.java
@@ -14,6 +14,7 @@ public abstract class AbstractPage {
     public AbstractPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.manage().window().maximize();
         PageFactory.initElements(driver, this);
     }
 
--- src/test/java/abstracted/AbstractTest.java
+++ src/test/java/abstracted/AbstractTest.java
@@ -1,19 +1,44 @@
 package abstracted;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class AbstractTest {
 
     protected WebDriver driver;
 
     @BeforeTest
-    public void setDriver() {
+    public void setDriver() throws MalformedURLException {
+        System.out.println(Boolean.getBoolean("use.selenium.grid"));
+        if (Boolean.getBoolean("use.selenium.grid"))
+            this.driver = this.getRemoteDriver();
+        else
+            this.driver = this.getLocalDriver();
+    }
+
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities = new ChromeOptions();
+        String url = "http://localhost:4444/wd/hub";
+
+        if (System.getProperty("browser").equalsIgnoreCase("firefox"))
+            capabilities = new FirefoxOptions();
+
+        return new RemoteWebDriver(new URL(url), capabilities);
+    }
+
+    private WebDriver getLocalDriver() {
         WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
+        return new ChromeDriver();
     }
 
     @AfterTest
